290cf3a513a1a068ee9459ef22887b5261eccb84
Meissaxd <briede.marija4@gmail.com>
2025-04-23 23:49:10 +0300

    leaderboard

--- SkiGame/Assets/Scripts/Leaderboard.cs
+++ SkiGame/Assets/Scripts/Leaderboard.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Leaderboard : MonoBehaviour
 {
     [SerializeField] private List<float> bestTimes = new();
+    [SerializeField] private List<TextMeshPro> timeTexts= new (); //text reference
 
     private void Awake()
     {
@@ -13,6 +16,8 @@ public class Leaderboard : MonoBehaviour
         {
            bestTimes.Add(PlayerPrefs.GetFloat("time"+ i, 9999999)) ;
         }
+
+        UpdateLeaderboardUI();
     }
 
     public void AddTime(float time)
@@ -20,6 +25,7 @@ public class Leaderboard : MonoBehaviour
         bestTimes.Add(time);
         bestTimes.Sort();
         SaveData();
+        UpdateLeaderboardUI();  //added for leaderboard
     }
 
     private void SaveData()
@@ -31,5 +37,27 @@ public class Leaderboard : MonoBehaviour
         }
         
         PlayerPrefs.Save();
+    }
+
+    private void UpdateLeaderboardUI() //added for leaderboard
+    {
+        for (int i = 0; i < timeTexts.Count; i++)
+        if (i < bestTimes.Count)
+        {
+            timeTexts[i].text = FormatTime(bestTimes[i]);
+        }
+        else
+        {
+            timeTexts[i].text = "---";
+        }
+    }
+
+    private string FormatTime(float time) //added for leaderboard
+    {
+        if (time >= 9999999) return "---";
+        int minutes = Mathf.FloorToInt(time / 60F);
+        int seconds = Mathf.FloorToInt(time % 60F);
+        int milliseconds = Mathf.FloorToInt((time * 1000) % 1000);
+        return $"{minutes: 00}:{seconds:00}.{milliseconds:000}";
     }
 }
